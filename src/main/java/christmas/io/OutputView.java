23f99a9364e7195573cdbc17bbf18556bb953db9
김승진 <ohksj77@gmail.com>
2023-11-09 18:40:37 +0900

    feat(OutputView): 인사 문구 출력 기능 구현

---
+++ src/main/java/christmas/io/OutputView.java
@@ -0,0 +1,10 @@
+package christmas.io;
+
+import christmas.constant.ProgressMessage;
+
+public class OutputView {
+
+    public void printGreeting() {
+        System.out.println(ProgressMessage.GREETING);
+    }
+}
