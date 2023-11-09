5f083d1abbc5e0d1248a1438f92b3f3c99197841
김승진 <ohksj77@gmail.com>
2023-11-09 19:46:17 +0900

    feat(InputValidator): 메뉴와 개수 입력값 형식 검증 기능 구현

--- src/main/java/christmas/io/InputValidator.java
+++ src/main/java/christmas/io/InputValidator.java
@@ -2,7 +2,11 @@ package christmas.io;
 
 import christmas.constant.ErrorMessage;
 
+import java.util.regex.Pattern;
+
 public class InputValidator {
+
+    private static final Pattern ORDER_MENUS_PATTER = Pattern.compile("([가-힣]+-[0-9]+,?)+");
 
     public void validateVisitDate(final String input) {
         if (isNotNumeric(input)) {
@@ -12,5 +16,11 @@ public class InputValidator {
 
     private boolean isNotNumeric(final String input) {
         return !input.chars().allMatch(Character::isDigit);
+    }
+
+    public void validateOrderMenus(final String input) {
+        if (!ORDER_MENUS_PATTER.matcher(input).matches()) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_ORDER_MENUS.toString());
+        }
     }
 }
