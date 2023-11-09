beb129f73cf0a8b4e9c7f244d4d912e8d5229d06
김승진 <ohksj77@gmail.com>
2023-11-09 20:38:16 +0900

    refactor(VisitDate): 숫자 static final 상수화

--- src/main/java/christmas/domain/VisitDate.java
+++ src/main/java/christmas/domain/VisitDate.java
@@ -5,16 +5,18 @@ import christmas.constant.ErrorMessage;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 
-public class VisitDate {
+public final class VisitDate {
 
     private static final int MIN_DATE = 1;
     private static final int MAX_DATE = 31;
+    private static final int CURRENT_YEAR = 2023;
+    private static final int CURRENT_MONTH = 12;
     private final LocalDate date;
     private final DayOfWeek dayOfWeek;
 
     public VisitDate(final int date) {
         validate(date);
-        this.date = LocalDate.of(2023, 12, date);
+        this.date = LocalDate.of(CURRENT_YEAR, CURRENT_MONTH, date);
         this.dayOfWeek = this.date.getDayOfWeek();
     }
 
