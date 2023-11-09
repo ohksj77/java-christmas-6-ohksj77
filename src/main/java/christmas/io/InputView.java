7f7f53adf89b5d16da15707c4878d4ae0d6c1f27
김승진 <ohksj77@gmail.com>
2023-11-09 19:56:29 +0900

    feat(InputView): 메뉴 입력 요청 메시지 출력 및 입력 기능 구현

--- src/main/java/christmas/io/InputView.java
+++ src/main/java/christmas/io/InputView.java
@@ -4,6 +4,8 @@ import camp.nextstep.edu.missionutils.Console;
 
 import christmas.constant.ProgressMessage;
 
+import java.util.function.Consumer;
+
 public class InputView {
 
     private final InputValidator inputValidator;
@@ -14,8 +16,17 @@ public class InputView {
 
     public String readVisitDate() {
         System.out.println(ProgressMessage.VISIT_DAY_REQUEST);
+        return readInput(inputValidator::validateVisitDate);
+    }
+
+    public String readOrderMenus() {
+        System.out.println(ProgressMessage.MENUS_REQUEST);
+        return readInput(inputValidator::validateOrderMenus);
+    }
+
+    private String readInput(final Consumer<String> validation) {
         final String input = Console.readLine();
-        inputValidator.validateVisitDate(input);
+        validation.accept(input);
         return input;
     }
 }
