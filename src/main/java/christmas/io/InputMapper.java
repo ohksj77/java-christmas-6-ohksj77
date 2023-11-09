d67037e10ef645e6a65dfe359cd129609a8c72cd
김승진 <ohksj77@gmail.com>
2023-11-09 19:56:45 +0900

    feat(InputMapper): 입력값 주문메뉴 도메인으로 변환 기능 구현

--- src/main/java/christmas/io/InputMapper.java
+++ src/main/java/christmas/io/InputMapper.java
@@ -1,10 +1,31 @@
 package christmas.io;
 
+import christmas.constant.Menu;
+import christmas.domain.OrderMenu;
+import christmas.domain.OrderMenus;
 import christmas.domain.VisitDate;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class InputMapper {
 
+    private static final String MENUS_DELIMITER = ",";
+    private static final String MENU_PRICE_DELIMITER = "-";
+
     public VisitDate toVisitDate(final String input) {
         return new VisitDate(Integer.parseInt(input));
+    }
+
+    public OrderMenus toOrderMenus(final String input) {
+        return Arrays.stream(input.split(MENUS_DELIMITER))
+                .map(this::toOrderMenu)
+                .collect(Collectors.collectingAndThen(Collectors.toList(), OrderMenus::new));
+    }
+
+    private OrderMenu toOrderMenu(final String string) {
+        final String[] strings = string.split(MENU_PRICE_DELIMITER);
+        final Menu menu = Menu.valueOfName(strings[0]);
+        return new OrderMenu(menu, Integer.valueOf(strings[1]));
     }
 }
