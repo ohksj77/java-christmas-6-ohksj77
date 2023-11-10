9c5ac9d95e7ecc6db11535f0ae22a80d91d4b154
김승진 <ohksj77@gmail.com>
2023-11-11 00:06:35 +0900

    feat(Difference): 차액 양수 여부 검증 기능 구현

--- src/main/java/christmas/constant/ErrorMessage.java
+++ src/main/java/christmas/constant/ErrorMessage.java
@@ -4,7 +4,9 @@ public enum ErrorMessage {
     INVALID_DATE("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
     NOT_NUMERIC("숫자를 입력해주세요."),
     NOT_SAVED("저장되지 않은 객체의 조회를 시도했습니다."),
-    INVALID_ORDER_MENUS("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    INVALID_ORDER_MENUS("유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    NOT_POSITIVE_MONEY("금액은 음수가 될 수 없습니다."),
+    NOT_POSITIVE_DIFFERENCE("차액은 음수가 될 수 없습니다.");
 
     private static final String PREFIX = "[ERROR] ";
     private final String message;
