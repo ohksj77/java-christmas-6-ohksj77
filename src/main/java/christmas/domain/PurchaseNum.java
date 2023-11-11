3ebdaa64beb056ba9b57217f213680305958e987
김승진 <ohksj77@gmail.com>
2023-11-11 11:14:37 +0900

    feat(PurchaseNum): 구매 개수 양수 여부 검증

--- src/main/java/christmas/domain/PurchaseNum.java
+++ src/main/java/christmas/domain/PurchaseNum.java
@@ -1,11 +1,21 @@
 package christmas.domain;
 
+import christmas.constant.ErrorMessage;
+
 public final class PurchaseNum {
 
+    private static final int MIN_PURCHASE_NUM = 1;
     private final Integer value;
 
     public PurchaseNum(final Integer value) {
+        validate(value);
         this.value = value;
+    }
+
+    private void validate(final Integer value) {
+        if (value < MIN_PURCHASE_NUM) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_ORDER_MENUS.toString());
+        }
     }
 
     public int toValue() {
